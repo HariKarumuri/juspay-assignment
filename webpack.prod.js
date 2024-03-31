ed11339ae041fcf25f299ec996da7bdb21b236fb
Hari <harikarumuri1086@gmail.com>
2024-04-01 00:58:13 +0530

    build added

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
@@ -8,11 +9,13 @@ module.exports = merge([
   common,
   {
     mode: "production",
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
     optimization: {
       minimize: true,
       minimizer: [
-        // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-        // `...`,
         new CssMinimizerPlugin(),
       ],
     },
